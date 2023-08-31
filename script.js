da87651574af83acbd17058e9ed9a3951363ae2d
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-08-31 09:47:31 -0700

    com

--- script.js
+++ script.js
@@ -1,14 +1,162 @@
 var inp1 = prompt('ENETR YOUR NAME')
 alert('ASALAM-O-ALAIKUM'+' '+ inp1);
-var name1 = prompt('ENETR YOUR FIRST SUBJECT NAME :')
-var name2 = prompt('ENETR YOUR SECOND SUBJECT NAME :')
-var name3 = prompt('ENETR YOUR THIRD SUBJECT NAME :')
 
-var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
-var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
-var mark3 = +prompt('ENETR YOUR'+' '+ name3 +' '+ 'MARKS')
-var totsl =  mark1 + mark2 + mark3
+var numLena = +prompt('ENTER YOUR SUBJECTS NUMBER');
+
+if(numLena==1){
+
+        var name1 = prompt('ENETR YOUR FIRST SUBJECT NAME :')
+    }
+    else if(numLena==2){
+        var name1 = prompt('ENETR YOUR FIRST SUBJECT NAME :')
+        var name2 = prompt('ENETR YOUR SECOND SUBJECT NAME :')
+    }
+    else if(numLena==3){
+        var name1 = prompt('ENETR YOUR FIRST SUBJECT NAME :')
+        var name2 = prompt('ENETR YOUR SECOND SUBJECT NAME :')
+        var name3 = prompt('ENETR YOUR THIRD SUBJECT NAME :')
+    }
+    else if(numLena==4){
+        var name1 = prompt('ENETR YOUR FIRST SUBJECT NAME :')
+        var name2 = prompt('ENETR YOUR SECOND SUBJECT NAME :')
+        var name3 = prompt('ENETR YOUR THIRD SUBJECT NAME :')
+        var name4 = prompt('ENETR YOUR FOURTH SUBJECT NAME :')
+    }
+    else if(numLena==5){
+        var name1 = prompt('ENETR YOUR FIRST SUBJECT NAME :')
+        var name2 = prompt('ENETR YOUR SECOND SUBJECT NAME :')
+        var name3 = prompt('ENETR YOUR THIRD SUBJECT NAME :')
+        var name4 = prompt('ENETR YOUR FOURTH SUBJECT NAME :')
+        var name5 = prompt('ENETR YOUR FIFTH SUBJECT NAME :')
+    }
+    else if(numLena==6){
+        var name1 = prompt('ENETR YOUR FIRST SUBJECT NAME :')
+        var name2 = prompt('ENETR YOUR SECOND SUBJECT NAME :')
+        var name3 = prompt('ENETR YOUR THIRD SUBJECT NAME :')
+        var name4 = prompt('ENETR YOUR FOURTH SUBJECT NAME :')
+        var name5 = prompt('ENETR YOUR FIFTH SUBJECT NAME :')
+        var name6 = prompt('ENETR YOUR SIXTH SUBJECT NAME :')
+    }
+    else if(numLena==7){
+        var name1 = prompt('ENETR YOUR FIRST SUBJECT NAME :')
+        var name2 = prompt('ENETR YOUR SECOND SUBJECT NAME :')
+        var name3 = prompt('ENETR YOUR THIRD SUBJECT NAME :')
+        var name4 = prompt('ENETR YOUR FOURTH SUBJECT NAME :')
+        var name5 = prompt('ENETR YOUR FIFTH SUBJECT NAME :')
+        var name6 = prompt('ENETR YOUR SIXTH SUBJECT NAME :')
+        var name7 = prompt('ENETR YOUR SEVEN SUBJECT NAME :')
+    }
+    else if(numLena==8){
+        var name1 = prompt('ENETR YOUR FIRST SUBJECT NAME :')
+        var name2 = prompt('ENETR YOUR SECOND SUBJECT NAME :')
+        var name3 = prompt('ENETR YOUR THIRD SUBJECT NAME :')
+        var name4 = prompt('ENETR YOUR FOURTH SUBJECT NAME :')
+        var name5 = prompt('ENETR YOUR FIFTH SUBJECT NAME :')
+        var name6 = prompt('ENETR YOUR SIXTH SUBJECT NAME :')
+        var name7 = prompt('ENETR YOUR SEVEN SUBJECT NAME :')
+        var name8 = prompt('ENETR YOUR EIGHT SUBJECT NAME :')
+    }
+    else{
+        var name1 = prompt('ENETR YOUR FIRST SUBJECT NAME :')
+        var name2 = prompt('ENETR YOUR SECOND SUBJECT NAME :')
+        var name3 = prompt('ENETR YOUR THIRD SUBJECT NAME :')
+        var name4 = prompt('ENETR YOUR FOURTH SUBJECT NAME :')
+        var name5 = prompt('ENETR YOUR FIFTH SUBJECT NAME :')
+        var name6 = prompt('ENETR YOUR SIXTH SUBJECT NAME :')
+        var name7 = prompt('ENETR YOUR SEVEN SUBJECT NAME :')
+        var name8 = prompt('ENETR YOUR EIGHT SUBJECT NAME :')
+        var name9 = prompt('ENETR YOUR NINE SUBJECT NAME :')
+    }
+    if(numLena==1){
+        var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
+        
+    }
+    else if(numLena==2){
+        var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
+        var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
+    
+    }
+    else if(numLena==3){
+        var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
+        var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
+        var mark3 = +prompt('ENETR YOUR'+' '+ name3 +' '+ 'MARKS')
+        
+    }
+    else if(numLena==4){
+        var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
+        var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
+        var mark3 = +prompt('ENETR YOUR'+' '+ name3 +' '+ 'MARKS')
+    
+        var mark4 = +prompt('ENETR YOUR'+' '+ name4 +' '+ 'MARKS')
+        
+    }
+    else if(numLena==5){
+        var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
+        var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
+        var mark3 = +prompt('ENETR YOUR'+' '+ name3 +' '+ 'MARKS')
+    
+        var mark4 = +prompt('ENETR YOUR'+' '+ name4 +' '+ 'MARKS')
+        
+        var mark5 = +prompt('ENETR YOUR'+' '+ name5 +' '+ 'MARKS')
+       
+    }
+    else if(numLena==6){
+        var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
+        var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
+        var mark3 = +prompt('ENETR YOUR'+' '+ name3 +' '+ 'MARKS')
+    
+        var mark4 = +prompt('ENETR YOUR'+' '+ name4 +' '+ 'MARKS')
+        
+        var mark5 = +prompt('ENETR YOUR'+' '+ name5 +' '+ 'MARKS')
+        var mark6 = +prompt('ENETR YOUR'+' '+ name6 +' '+ 'MARKS')
+        
+    }
+    else if(numLena==7){
+        var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
+        var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
+        var mark3 = +prompt('ENETR YOUR'+' '+ name3 +' '+ 'MARKS')
+    
+        var mark4 = +prompt('ENETR YOUR'+' '+ name4 +' '+ 'MARKS')
+        
+        var mark5 = +prompt('ENETR YOUR'+' '+ name5 +' '+ 'MARKS')
+        var mark6 = +prompt('ENETR YOUR'+' '+ name6 +' '+ 'MARKS')
+        var mark7 = +prompt('ENETR YOUR'+' '+ name7 +' '+ 'MARKS')
+       
+    }
+    else if(numLena==8){
+        var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
+        var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
+        var mark3 = +prompt('ENETR YOUR'+' '+ name3 +' '+ 'MARKS')
+    
+        var mark4 = +prompt('ENETR YOUR'+' '+ name4 +' '+ 'MARKS')
+        
+        var mark5 = +prompt('ENETR YOUR'+' '+ name5 +' '+ 'MARKS')
+        var mark6 = +prompt('ENETR YOUR'+' '+ name6 +' '+ 'MARKS')
+        var mark7 = +prompt('ENETR YOUR'+' '+ name7 +' '+ 'MARKS')
+        var mark8 = +prompt('ENETR YOUR'+' '+ name8 +' '+ 'MARKS')
+    }
+    else if(numLena==9){
+        var mark1 = +prompt('ENETR YOUR'+' '+ name1 +' '+ 'MARKS')
+        var mark2 = +prompt('ENETR YOUR'+' '+ name2 +' '+ 'MARKS')
+        var mark3 = +prompt('ENETR YOUR'+' '+ name3 +' '+ 'MARKS')
+    
+        var mark4 = +prompt('ENETR YOUR'+' '+ name4 +' '+ 'MARKS')
+        
+        var mark5 = +prompt('ENETR YOUR'+' '+ name5 +' '+ 'MARKS')
+        var mark6 = +prompt('ENETR YOUR'+' '+ name6 +' '+ 'MARKS')
+        var mark7 = +prompt('ENETR YOUR'+' '+ name7 +' '+ 'MARKS')
+        var mark8 = +prompt('ENETR YOUR'+' '+ name8 +' '+ 'MARKS')
+        var mark9 = +prompt('ENETR YOUR'+' '+ name9 +' '+ 'MARKS')
+    }
+    else{
+        console.log('abc')
+    }
+var totsl =numLena
 var total = +prompt('3 SUBJECT KE TOTAL MARK DALO DEKO AGR 1 SUB KE 100 HAIN TO 3 KE 300 DALOOO')
 var perc = (totsl / 300) * 100 
- document.write('YOUR TOTAL MARKS OF ALL SUBJECT IS'+' '+ totsl+' '+'OUT OF'+' '+total+' '+'AND YOUR PERCENTAGE IS'
+ document.write('YOUR TOTAL MARKS OF ALL SUBJECT IS'+' '+ totsl+' '+'OUT OF'+' '+total+' '+'AND YOUR PERCENTAGE IS')
+var perc = (numLena / 300) * 100 
+ document.write('YOUR TOTAL MARKS OF ALL SUBJECT IS'+' '+numLena+' '+'OUT OF'+' '+total+' '+'AND YOUR PERCENTAGE IS'
  +' '+perc.toFixed(2)+"%"+ ' '+ 'OUT OF 100%')
+
+// 
